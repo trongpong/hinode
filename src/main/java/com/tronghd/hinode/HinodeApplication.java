5d500d51b1707366263f955f28b820ecdeb1dd69
trongpong <tronghd01884@gmail.com>
2018-08-17 22:03:13 +0900

    initial commit

---
+++ src/main/java/com/tronghd/hinode/HinodeApplication.java
@@ -0,0 +1,12 @@
+package com.tronghd.hinode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinodeApplication.class, args);
+	}
+}

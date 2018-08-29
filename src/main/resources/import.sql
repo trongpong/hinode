0127d56c58edcffa4591a6c890f6028a5488fb39
trongpong <tronghd01884@gmail.com>
2018-08-29 23:01:12 +0900

    Update Init sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC1', 'tokyo1', 'manson', 70000, 35000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC2', 'tokyo2', 'manson', 71000, 34000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC3', 'tokyo3', 'apartment', 72000, 33000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC4', 'tokyo4', 'manson', 73000, 32000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC5', 'tokyo5', 'manson', 74000, 31000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC6', 'tokyo6', 'apartment', 75000, 30000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC7', 'tokyo7', 'apartment', 76000, 39000, 52.2, '0');
-INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg) VALUES('ABC8', 'tokyo8', 'manson', 77000, 38000, 52.2, '0');
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC1', 'tokyo1', 'manson', 70000, 35000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC2', 'tokyo2', 'manson', 71000, 34000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC3', 'tokyo3', 'apartment', 72000, 33000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC4', 'tokyo4', 'manson', 73000, 32000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC5', 'tokyo5', 'manson', 74000, 31000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC6', 'tokyo6', 'apartment', 75000, 30000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC7', 'tokyo7', 'apartment', 76000, 39000, 52.2, '0', 0, 0);
+INSERT INTO house(meisho, jusho, type, chinryo, reikin, menseki, del_flg, henkoryo, shikikin) VALUES('ABC8', 'tokyo8', 'manson', 77000, 38000, 52.2, '0', 0, 0);
